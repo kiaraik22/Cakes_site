6001cc283d839303e14e3c5c7dded8da4405d1e5
kiaraik22 <kora2206@mail.ru>
2025-10-06 17:21:24 +0300

    изменения от 5 октября

--- cakes/menu/urls.py
+++ cakes/menu/urls.py
@@ -5,4 +5,8 @@ from . import views
 urlpatterns = [
     path('', views.index, name='menu.index'),
 
+    path('catalog/', views.catalog, name='menu.catalog'),
+
+    path('flower-detail/<str:pk>', views.flower_details, name='flower_details'),
+
 ]
--- cakes/menu/views.py
+++ cakes/menu/views.py
@@ -1,12 +1,27 @@
 
 from django.shortcuts import render
 from menu.models import MenuCake
+from blog.models import Posts
+
 
 
 # Create your views here.
 
 def index(request):
     cake = MenuCake.objects.all()
+    posts = Posts.objects.all()
 
-    context = {'cake': cake}
+    context = {'cake': cake, 'posts': posts}
     return render(request, 'menu/index.html', context)
+
+def catalog(request):
+    flowers = MenuCake.objects.all()
+
+    context = {'flowers': flowers}
+
+    return render(request, 'menu/shop-fullwidth-list.html', context)
+
+def flower_details(request, pk):
+    flowers_d = MenuCake.objects.get(pk=pk)
+    context = {'flowers_d': flowers_d}
+    return render(request,'menu/product-details.html', context)
